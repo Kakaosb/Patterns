7b082724147067900f7ba6c86e7f7021d25f7253
kakaosb <kakaosb@yandex.ru>
2021-08-08 18:32:59 +0300

    Flyweight Pattern

---
+++ Patterns/Contexts/Structural/FlyweightContext.cs
@@ -0,0 +1,60 @@
+﻿using Patterns.Impl.Structural.Flyweight;
+using System;
+
+namespace Patterns.Contexts
+{
+    public class FlyweightContext : IContext
+    {
+        public void Excecute()
+        {
+            // Клиентский код обычно создает кучу предварительно заполненных
+            // легковесов на этапе инициализации приложения.
+            var factory = new FlyweightFactory(
+                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+                new Car { Company = "BMW", Model = "M5", Color = "red" },
+                new Car { Company = "BMW", Model = "X6", Color = "white" }
+            );
+            factory.ListFlyweights();
+
+            AddCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "M5",
+                Color = "red"
+            });
+
+            AddCarToPoliceDatabase(factory, new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Company = "BMW",
+                Model = "X1",
+                Color = "red"
+            });
+
+            factory.ListFlyweights();
+
+            Console.WriteLine();
+        }
+
+        private static void AddCarToPoliceDatabase(FlyweightFactory factory, Car car)
+        {
+            Console.WriteLine("\nClient: Adding a car to database.");
+
+            var flyweight = factory.GetFlyweight(new Car
+            {
+                Color = car.Color,
+                Model = car.Model,
+                Company = car.Company
+            });
+
+            // Клиентский код либо сохраняет, либо вычисляет внешнее состояние и
+            // передает его методам легковеса.
+            flyweight.Operation(car);
+        }
+    }
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -29,7 +29,8 @@ namespace Patterns
              new Pattern() { Discription = "Состояние", GetContextAction = () => new StateContext(),  Key = "state" },
              new Pattern() { Discription = "Мост", GetContextAction = () => new BridgeContext(),  Key = "bridge" },
              new Pattern() { Discription = "Строитель", GetContextAction = () => new BuilderContext(),  Key = "builder" },
-             new Pattern() { Discription = "Прототип", GetContextAction = () => new PrototypeContext(),  Key = "prototype" }
+             new Pattern() { Discription = "Прототип", GetContextAction = () => new PrototypeContext(),  Key = "prototype" },
+             new Pattern() { Discription = "Легковес", GetContextAction = () => new FlyweightContext(),  Key = "flyweight" }
          };
 
         static void Main(string[] args)
